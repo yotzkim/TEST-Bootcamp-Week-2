af92139e74dca3fb78765ca96ce4f90dca8f193c
yotzkim <yongtokim1215@gmail.com>
2023-02-06 11:52:34 -0800

    2/6

---
+++ js/list.js
@@ -0,0 +1,12 @@
+const image = document.getElementById("cheesecake");
+const image2 = document.getElementById("cookiesandcream");
+
+let degree = 0;
+let degree2 = 0;
+
+setInterval(function () {
+  degree = (degree + 2) % 360;
+  degree2 = (degree2 + 2) % 360;
+  image.style.transform = `rotate(${degree}deg)`;
+  image2.style.transform = `rotate(${degree}deg)`;
+}, 20)
